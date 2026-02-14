ab8df387c2c25683646cfb39edc7547f2ed84f63
WilsonCSilva <wilso.carvalhais@gmail.com>
2026-02-14 15:59:19 -0300

    Inclusão Teste Unitário PropriedadeServiceTests

---
+++ PropriedadeService.Tests/Controllers/PropriedadeControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using FluentAssertions;
+using Moq;
+using PropriedadeService.PropriedadeController;
+using PropriedadeService.Repositories.Interface;
+using PropriedadeService.Models;
+using Xunit;
+
+namespace PropriedadeServiceTests.Controllers;
+
+public class PropriedadeControllerTests
+{
+    private readonly Mock<IPropriedadeRepository> _repositoryMock;
+    private readonly PropriedadeController _controller;
+
+    public PropriedadeControllerTests()
+    {
+        _repositoryMock = new Mock<IPropriedadeRepository>();
+        _controller = new PropriedadeController(_repositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task CriarPropriedadeAsync_DeveChamarRepository()
+    {
+        var propriedade = new Propriedade
+        {
+            Nome = "Fazenda FIAP SP",
+            Cidade = "Ribeirão Preto",
+            Estado = "SP",
+            AreaHectares = 120
+        };
+
+        await _controller.CriarPropriedadeAsync(propriedade);
+
+        _repositoryMock.Verify(r =>
+            r.AddAsync(It.IsAny<Propriedade>()),
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task ListarPropriedadeAsync_DeveRetornarLista()
+    {
+        var lista = new List<Propriedade>
+        {
+            new Propriedade { Id = 1, Nome = "Fazenda FIAP SP" }
+        };
+
+        _repositoryMock
+            .Setup(r => r.GetAllAsync())
+            .ReturnsAsync(lista);
+
+        var resultado = await _controller.ListarPropriedadeAsync();
+
+        resultado.Should().HaveCount(1);
+        resultado.First().Nome.Should().Be("Fazenda FIAP SP");
+    }
+
+    [Fact]
+    public async Task BuscarPorIdAsync_DeveRetornarPropriedade()
+    {
+        var propriedade = new Propriedade
+        {
+            Id = 10,
+            Nome = "Fazenda X"
+        };
+
+        _repositoryMock
+            .Setup(r => r.GetByIdAsync(10))
+            .ReturnsAsync(propriedade);
+
+        var resultado = await _controller.BuscarPorIdAsync(10);
+
+        resultado.Should().NotBeNull();
+        resultado.Id.Should().Be(10);
+    }
+
+    [Fact]
+    public async Task RemoverPropriedadeAsync_DeveChamarRepository()
+    {
+        await _controller.RemoverPropriedadeAsync(5);
+
+        _repositoryMock.Verify(r =>
+            r.DeleteAsync(5),
+            Times.Once);
+    }
+}
---
+++ PropriedadeService.Tests/Repositories/PropriedadeRepositoryTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using PropriedadeService.Data;
+using PropriedadeService.Models;
+using PropriedadeService.Repositories;
+using Xunit;
+
+namespace PropriedadeServiceTests.Repositories;
+
+public class PropriedadeRepositoryTests
+{
+    private PropriedadesDbContext CriarContexto()
+    {
+        var options = new DbContextOptionsBuilder<PropriedadesDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        return new PropriedadesDbContext(options);
+    }
+
+    [Fact]
+    public async Task CriarAsync_DeveSalvarNoBanco()
+    {
+        var context = CriarContexto();
+        var repository = new PropriedadeRepository(context);
+
+        var propriedade = new Propriedade
+        {
+            Nome = "Fazenda FIAP RJ",
+            Cidade = "Rio de Janeiro",
+            Estado = "RJ",
+            AreaHectares = 500
+        };
+
+        await repository.AddAsync(propriedade);
+
+        var salvo = await context.Propriedades.FirstOrDefaultAsync();
+
+        salvo.Should().NotBeNull();
+        salvo.Nome.Should().Be("Fazenda FIAP RJ");
+    }
+
+    [Fact]
+    public async Task ListarAsync_DeveRetornarDados()
+    {
+        var context = CriarContexto();
+
+        context.Propriedades.Add(new Propriedade
+        {
+            Nome = "Fazenda FIAP SP",
+            Cidade = "Ribeirão Preto", 
+            Estado = "SP",           
+            AreaHectares = 400         
+        });
+
+        await context.SaveChangesAsync();
+
+        var repository = new PropriedadeRepository(context);
+
+        var lista = await repository.GetAllAsync();
+
+        lista.Should().NotBeNull();
+        lista.Should().HaveCount(1);
+        lista.First().Nome.Should().Be("Fazenda FIAP SP");
+    }
+}
