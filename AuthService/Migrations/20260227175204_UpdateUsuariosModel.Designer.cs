b3b255510ea93c9136662cec5e0ae759aaa5c55d
Rafael Henrique Vasconcelos <vasconcelosrafaelp2@johndeere.com>
2026-02-27 14:53:58 -0300

    fix: migrations

--- AuthService/Migrations/20260227175204_UpdateUsuariosModel.Designer.cs
+++ AuthService/Migrations/20260227175204_UpdateUsuariosModel.Designer.cs
@@ -3,6 +3,7 @@ using AuthService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthService.Migrations
 {
     [DbContext(typeof(UsuariosDbContext))]
-    partial class UsuariosDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260227175204_UpdateUsuariosModel")]
+    partial class UpdateUsuariosModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AuthService/Migrations/20260227175204_UpdateUsuariosModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUsuariosModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SenhaHash",
+                table: "Usuario",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SenhaHash",
+                table: "Usuario",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+        }
+    }
+}
